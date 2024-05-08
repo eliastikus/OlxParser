2a08581452871852b6a74632be831b90745b5626
Illia <eliastikus@gmail.com>
2024-05-08 14:42:35 +0200

    Last version

--- public/script.js
+++ public/script.js
@@ -1,73 +1,217 @@
-function applyFilter() {
-    const loader = document.querySelector('.loader');
-    const table = document.querySelector('#listings-table');
-    const selectedOption = document.getElementById('category-dropdown').options[document.getElementById('category-dropdown').selectedIndex];
-    const selectedCategory = selectedOption.dataset.urlending;
-    const minPrice = parseFloat(document.getElementById('min-price').value);
-    const maxPrice = parseFloat(document.getElementById('max-price').value);
-
-    if (selectedCategory) {
-        showLoader(loader, table);
-        const url = 'https://www.olx.pl/elektronika/komputery/podzespoly-i-czesci/q-' + selectedCategory + '/';
-        fetchListings(url, minPrice, maxPrice, loader, table);
-    } else {
-        clearListings(table, loader);
-    }
-}
-
-function showLoader(loader, table) {
-    loader.style.display = 'block';
+const gpuModels = [
+    {
+      name: '3050',
+      imgURL: 'https://www.gigabyte.com/FileUpload/Global/KeyFeature/3648/innergigabyteimages/kf-img.png',
+    },
+    {
+      name: '3060',
+      imgURL: 'https://www.gigabyte.com/FileUpload/Global/KeyFeature/2160/innergigabyteimages/kf-img.png',
+    },
+    {
+      name: '3060 Ti',
+      imgURL: 'https://www.gigabyte.com/FileUpload/Global/KeyFeature/1910/innergigabyteimages/kf-img.png',
+    },
+    {
+      name: '3070',
+      imgURL:
+        'https://www.nvidia.com/content/dam/en-zz/Solutions/geforce/ampere/rtx-3070/geforce-rtx-3070-shop-600-p@2x.png',
+    },
+    {
+      name: '3070 Ti',
+      imgURL: 'https://www.gigabyte.com/FileUpload/Global/KeyFeature/1881/innergigabyteimages/kf-img.png',
+    },
+    {
+      name: '6500 XT',
+      imgURL: 'https://dlcdnwebimgs.asus.com/gain/4d7af692-c98e-43c0-8550-1573dcdde294/',
+    },
+    {
+      name: '6600',
+      imgURL: 'https://www.powercolor.com/_upload/images//2106211414120.png',
+    },
+    {
+      name: '6600 XT',
+      imgURL: 'https://www.amd.com/system/files/2021-07/910022-amd-radeon-6600XT-angle1260x709_0.png',
+    },
+    {
+      name: '6650 XT',
+      imgURL: 'https://a-power.com/app/uploads/2022/07/99873611_6303858573.png',
+    },
+    {
+      name: '6700 XT',
+      imgURL: 'https://dlcdnwebimgs.asus.com/gain/643cb170-7b45-4ce6-a59e-af140ce66c2c/w800',
+    },
+    {
+      name: '6750 XT',
+      imgURL: 'https://dlcdnwebimgs.asus.com/gain/FBB83F06-C7D1-42B5-9690-229B3A4C0A94/w717/h525',
+    },
+  ];
+  
+  let currentData = []; // Holds the current unfiltered data
+  
+  function showLoader(loader, table) {
+    loader.style.display = 'flex';
     table.style.display = 'none';
-}
-
-function fetchListings(url, minPrice, maxPrice, loader, table) {
-    fetch('/listings?link=' + encodeURIComponent(url))
-        .then(response => response.json())
-        .then(data => {
-            const tbody = document.querySelector('#listings-table tbody');
-            tbody.innerHTML = '';
-            data.forEach(item => {
-                displayListing(item, minPrice, maxPrice, tbody);
-            });
-            hideLoader(loader, table);
-        })
-        .catch(error => console.error('Error fetching data:', error));
-}
-
-function displayListing(item, minPrice, maxPrice, tbody) {
-    const cityParts = item.city.split('-');
-    const city = cityParts[0].trim();
-    const priceParts = item.price.split('.')[0].trim(); 
-    const priceValue = parseFloat(priceParts.replace(/[^\d.-]/g, '')); 
-    if (!isNaN(priceValue) && (isNaN(minPrice) || priceValue >= minPrice) && (isNaN(maxPrice) || priceValue <= maxPrice)) {
-        const price = priceParts.replace('zł', 'zł  ');
-        const tr = document.createElement('tr');
-        
-        const link = document.createElement('a');
-        link.textContent = item.title;
-        link.href = item.url;
-
-        const tdTitle = document.createElement('td');
-        tdTitle.appendChild(link);
-        tr.appendChild(tdTitle);
-
-        tr.innerHTML += '<td>' + price + '</td>' +
-                        '<td class="city">' + city + '</td>';
-        
-        tbody.appendChild(tr);
-    }
-}
-
-
-function hideLoader(loader, table) {
+  }
+  
+  function hideLoader(loader, table) {
     loader.style.display = 'none';
     table.style.display = 'table';
-}
-
-function clearListings(table, loader) {
+  }
+  
+  function clearListings(table, loader) {
     document.querySelector('#listings-table tbody').innerHTML = '';
     loader.style.display = 'none';
     table.style.display = 'table';
-}
-
-document.getElementById('apply-filter').addEventListener('click', applyFilter);
+  }
+  
+  function changeGPUImage(gpu) {
+    // Find the corresponding GPU model object
+    const gpuModel = gpuModels.find((model) => model.name === gpu);
+  
+    // Update the image source if a corresponding GPU model is found
+    if (gpuModel) {
+      const imgElement = document.getElementById('gpu-img');
+      imgElement.src = gpuModel.imgURL;
+      imgElement.alt = 'Image of ' + gpuModel.name;
+    }
+  }
+  
+  function fetchListings(url, minPrice, maxPrice, loader, table) {
+    showLoader(loader, table); // Show loader when fetching data
+  
+    const fullUrl = new URL(url);
+  
+    if (minPrice) fullUrl.searchParams.append('search[filter_float_price:from]', minPrice);
+    if (maxPrice) fullUrl.searchParams.append('search[filter_float_price:to]', maxPrice);
+  
+    console.log('Fetching data from:', fullUrl.href);
+  
+    fetch(`/listings?link=${encodeURIComponent(fullUrl.href)}`)
+      .then((response) => response.json())
+      .then((data) => {
+        currentData = data; // Store fetched data
+        populateTable(data); // Populate table without filtering
+        hideLoader(loader, table); // Hide loader after data is fetched
+      })
+      .catch((error) => {
+        console.error('Error fetching data:', error);
+        hideLoader(loader, table); // Hide loader if fetch fails
+      });
+  }
+  
+  function populateTable(data) {
+    const tbody = document.querySelector('#listings-table tbody');
+    tbody.innerHTML = ''; // Clear existing rows
+    data.forEach((item) => {
+      displayListing(item, tbody);
+    });
+  }
+  
+  function displayListing(item, tbody) {
+    const tr = document.createElement('tr');
+  
+    // Add a class to the row if the item should be filtered out
+    if (item.filteredOut) {
+      tr.classList.add('filtered-out');
+    }
+  
+    const link = document.createElement('a');
+    link.textContent = item.title;
+    link.href = item.url;
+    link.target = '_blank'; // Link opens in a new tab
+  
+    const tdTitle = document.createElement('td');
+    tdTitle.appendChild(link);
+    tr.appendChild(tdTitle);
+  
+    const price = item.price.split('.')[0].trim();
+    const priceTd = document.createElement('td');
+    priceTd.textContent = price;
+    priceTd.classList.add('price');
+    tr.appendChild(priceTd);
+  
+    const cityTd = document.createElement('td');
+    cityTd.classList.add('city');
+    cityTd.textContent = item.city;
+  
+    // Check if the city name includes 'Kraków' and add the 'rainbow' class
+    if (item.city.includes('Kraków')) {
+      cityTd.classList.add('rainbow');
+    }
+  
+    tr.appendChild(cityTd);
+  
+    tbody.appendChild(tr);
+  }
+  
+  function applyFilter() {
+    const loader = document.querySelector('.loader');
+  
+    const table = document.querySelector('#listings-table');
+    const selectedOption =
+      document.getElementById('category-dropdown').options[document.getElementById('category-dropdown').selectedIndex];
+  
+    changeGPUImage(selectedOption.value);
+    const selectedCategory = selectedOption.dataset.urlending;
+    const minPrice = parseFloat(document.getElementById('min-price').value);
+    const maxPrice = parseFloat(document.getElementById('max-price').value);
+  
+    if (selectedCategory) {
+      const url =
+        'https://www.olx.pl/elektronika/komputery/podzespoly-i-czesci/karty-graficzne/q-' + selectedCategory + '/';
+      fetchListings(url, minPrice, maxPrice, loader, table);
+    } else {
+      clearListings(table, loader);
+    }
+  }
+  
+  function filterIrrelevantResults() {
+    const selectedOption = document.getElementById('category-dropdown').selectedOptions[0];
+    const selectedCategory = selectedOption.value; // Using the value attribute
+    const selectedModel = gpuModels.find((model) => model.name === selectedCategory).name;
+  
+    // Generate possible variants of the model name
+  
+    const allPossibleVariants = gpuModels.flatMap((model) =>
+      model.name.split(' ').length > 1
+        ? [model.name.split(' ').join('-'), model.name.split(' ').join(' '), model.name.split(' ').join('')].map(
+            (variant) => variant.toLowerCase()
+          )
+        : model.name.toLowerCase()
+    );
+  
+    const possibleSelectedVariants = [
+      selectedModel.split(' ').join(''),
+      selectedModel.split(' ').join('-'),
+      selectedModel.split(' ').join(' '),
+    ].map((variant) => variant.toLowerCase());
+  
+    currentData.forEach((item) => {
+      const titleNormalized = item.title.toLowerCase();
+  
+      // Check if the title exactly matches any of the possible variants
+      const isRelevant = possibleSelectedVariants.some((variant) => {
+        return titleNormalized
+          .match(/[a-z0-9]+/g)
+          .join(' ')
+          .includes(variant);
+      });
+  
+      // Filter out entries based on the presence of other model names that could be supersets
+      const includesIncorrectModel = allPossibleVariants
+        .filter((variant) => possibleSelectedVariants.some((selected) => variant.includes(selected)))
+        .some((model) => {
+          return titleNormalized.includes(model) && !possibleSelectedVariants.includes(model);
+        });
+  
+      // Set filteredOut status
+      item.filteredOut = !isRelevant || includesIncorrectModel;
+    });
+  
+    populateTable(currentData); // Populate the table with the updated currentData
+  }
+  
+  document.getElementById('category-dropdown').addEventListener('change', applyFilter);
+  document.getElementById('apply-filter').addEventListener('click', applyFilter);
+  document.getElementById('filter-results').addEventListener('click', filterIrrelevantResults);
+  
--- server.js
+++ server.js
@@ -2,65 +2,63 @@ const express = require('express');
 const axios = require('axios');
 const cheerio = require('cheerio');
 const path = require('path');
+const querystring = require('querystring'); // Using the native querystring module
 
 const app = express();
 const port = 3001;
 
+app.use(express.static('public')); // It's better to place static middleware outside of any specific route to apply it globally
+
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
-    app.use(express.static('public'));
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.get('/listings', async (req, res) => {
-    const link = req.query.link;
+  const { link } = req.query;
 
-    if (!link) {
-        return res.status(400).send('No link provided');
-    }
+  if (!link) {
+    return res.status(400).send('No link provided');
+  }
 
-    let totalPages = 2;
-    let currentPage = 1;
+  try {
+    const response = await axios.get(decodeURIComponent(link));
+    const html = response.data;
+    const $ = cheerio.load(html);
 
-    try {
-        let allListings = [];
+    let listings = [];
 
-        while (currentPage <= totalPages) {
-            const response = await axios.get(`${link}?page=${currentPage}`);
-            const html = response.data;
-            const $ = cheerio.load(html);
+    $('.css-j0t2x2')
+      .first()
+      .find('.css-1sw7q4x')
+      .each((_, element) => {
+        const title = $(element).find('.css-16v5mdi.er34gjf0').text();
+        const price = $(element)
+          .find('.css-tyui9s.er34gjf0')
+          .contents()
+          .filter(function () {
+            return this.type === 'text';
+          })
+          .text()
+          .trim();
+        const cityParts = $(element).find('.css-1a4brun.er34gjf0').text().split('-');
+        const url = $(element).find('.css-z3gu2d').attr('href');
+        const city = cityParts[0].trim();
 
-            if ($('.css-pyu9k9').length > 0) {
-                totalPages++;
-            }
+        listings.push({
+          title,
+          price,
+          city,
+          url: 'https://www.olx.pl' + url,
+        });
+      });
 
-            const listings = [];
-
-            $('.css-1sw7q4x').each((index, element) => {
-                const title = $(element).find('.css-16v5mdi.er34gjf0').text();
-                const price = $(element).find('.css-tyui9s.er34gjf0').text();
-                const cityParts = $(element).find('.css-1a4brun.er34gjf0').text().split('-');
-                const url = $(element).find('.css-z3gu2d').attr('href');
-                const city = cityParts[0].trim();
-
-                listings.push({
-                    title: title,
-                    price: price,
-                    city: city,
-                    url: "https://www.olx.pl" + url
-                });
-            });
-
-            allListings = allListings.concat(listings);
-            currentPage++;
-        }
-
-        res.json(allListings);
-    } catch (error) {
-        console.error('Error loading page:', error);
-        res.status(500).send('Error loading page');
-    }
+    res.json(listings);
+  } catch (error) {
+    console.error('Error loading page:', error);
+    res.status(500).send('Error loading page');
+  }
 });
 
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Server is running on http://localhost:${port}`);
 });
